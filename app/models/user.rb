45fa34c3cf47b9c9c5402b423d582ffb92b9ba87
Kasun Thennakoon <tmkasun@gmail.com>
2013-09-21 08:58:14 +0530

    fix , attr_accessible removed coz rails 4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
   
-  attr_accessible :email , :password , :password_confirmation #http://stackoverflow.com/questions/3136420/difference-between-attr-accessor-and-attr-accessible
+  # with rails 4 we don't want this line coz we have strong_attributes
+  #attr_accessible :email , :password , :password_confirmation #http://stackoverflow.com/questions/3136420/difference-between-attr-accessor-and-attr-accessible
+  
   validate_uniqueness_of :email # or validate :email , uniqueness: true  
 end
