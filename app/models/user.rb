8ad3af3356a49a8b581f8f0783d8c95f89fb238c
Kasun Thennakoon <tmkasun@gmail.com>
2013-09-21 09:00:04 +0530

    fix , validate_uniqueness_of removed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
-  
+
   # with rails 4 we don't want this line coz we have strong_attributes
   #attr_accessible :email , :password , :password_confirmation #http://stackoverflow.com/questions/3136420/difference-between-attr-accessor-and-attr-accessible
-  
-  validate_uniqueness_of :email # or validate :email , uniqueness: true  
+
+  validate :email , uniqueness: true
 end
