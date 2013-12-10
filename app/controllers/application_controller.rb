3bf059742dd4e1d743e4f1b7fc05083ec460b312
Kasun Thennakoon <tmkasun@gmail.com>
2013-12-10 06:54:11 +0530

    add mobile loging page (dumy) page just for look and feel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :authenticate_user!
   before_filter :configure_permitted_parameters, if: :devise_controller?
-
+  before_filter :prepare_for_mobile
 
 
   protected
@@ -24,6 +24,29 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:registration_number,:password, :password_confirmation, :remember_me) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:registration_number, :password, :remember_me) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:registration_number, :password, :password_confirmation, :current_password) }
+  end
+  
+  def mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param] == "1"
+    else
+      request.user_agent =~ /Mobile|webOS/
+    end
+  end
+  
+  helper_method :mobile_device?
+  
+=begin
+helper_method is useful when the functionality is something that's used between both the controller and the view. A good example is something like current_user.
+
+If the method deals more with controller logic and not formatting then it belongs in the controller. Something like current_user would be shared between all controllers so it should be defined in the ApplicationController.
+
+True "helper" methods deal with the view and handle things like formatting and template logic. These are seldom needed in the controller and they belong in their own module under app/helpers. You can include these in your controller when needed, but you end up with the whole module worth of view helper methods available to your controller.
+=end
+  
+  def prepare_for_mobile
+    session[:mobile_param] = params[:mobile] if params[:mobile]
+    request.format = :mobile if mobile_device?
   end
 
 end
