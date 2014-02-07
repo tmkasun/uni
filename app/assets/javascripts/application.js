78ec9e2faada9deaa0f22fa9741725f23673ee6f
Kasun Thennakoon <tmkasun@gmail.com>
2014-02-07 12:40:04 +0530

    fix styling issues

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 //= require turbolinks
 //= require jquery.turbolinks
 //= require google_analytics
